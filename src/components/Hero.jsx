8c7e2d7d0d26ac85ace9d0ad9051a8f3585a0a65
AASHUPA <thakurdivy84@gmail.com>
2025-08-29 15:17:30 +0530

    wer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ export default function Hero() {
         playsInline
         className="absolute inset-0 w-full h-full object-cover -z-10"
       >
-        <source src="/hv.mp4" type="video/mp4" />
+        <source src="/VV.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
