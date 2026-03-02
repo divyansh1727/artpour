e0041126a6834737c7035734ff31bff5afe22b94
AASHUPA <thakurdivy84@gmail.com>
2026-03-02 20:02:25 +0530

    done3

--- src/api/products.js
+++ src/api/products.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 const API = "https://artpour.onrender.com/api/products";
-
 export const getProducts = () => axios.get(API);
 export const getProductById = (id) => axios.get(`${API}/${id}`);
